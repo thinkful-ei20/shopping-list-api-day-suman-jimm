d93c3b37cb2a1c0ef8f95e7c6e604436d3ed954a
Suman Bandita <sumanbandita@gmail.com>
2018-04-11 11:58:29 -0400

    getItems() method

--- scripts/api.js
+++ scripts/api.js
@@ -6,7 +6,8 @@ const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/sumanjimm';
 
   function getItems(callback){
-    callback('api module works!');
+    //callback('api module works!');
+    //$.getJSON(`${BASE_URL}/items`,callback);
   }
 
   return {
